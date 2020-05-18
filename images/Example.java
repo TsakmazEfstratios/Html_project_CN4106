ed30a93130b42f6aaaad44c6a401b01ab757b177
Efstratios Tsakmaz <tsakmazstratos@outlook.com.gr>
2020-05-18 16:55:47 +0300

    Working on
    To be continued

---
+++ images/Example.java
@@ -0,0 +1,14 @@
+public class Example{
+
+    public static void main(){
+
+        String fruit = "Apple";
+
+        for(int index=0; index<fruit.length(); index++){
+
+            char letter = fruit.charAt(index);
+            System.out.println(letter);    
+        }
+
+    }
+}
